47967dce84ee7b1f6f4a19cbaf494f173e98dd03
shenadevilla <97103094+shenadevilla@users.noreply.github.com>
2022-10-27 15:06:18 +0800

    Add files via upload

---
+++ FrmBasicThread.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BasicThreading
+{
+    public partial class FrmBasicThread : Form
+{
+        private Thread ThreadA, ThreadB;
+
+        public FrmBasicThread()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn1_Click(object sender, EventArgs e)
+        {
+            ThreadStart delThread = new ThreadStart(MyThreadClass.Thread1);
+            ThreadA = new Thread(delThread);
+            ThreadA.Name = "Thread A Process";
+            ThreadB = new Thread(delThread);
+            ThreadB.Name = "Thread B Process";
+
+            ThreadA.Start();
+            ThreadB.Start();
+
+            ThreadA.Join();
+            ThreadB.Join();
+            label1.Text = (" -End of Thread- ");
+            Console.WriteLine("End of Thread");
+        }
+
+        private void FrmBasicThread_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MyThreadClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BasicThreading
+{
+    class MyThreadClass
+    {
+        public static void Thread1()
+        {
+            for (int loopCount = 0; loopCount < 6; loopCount++)
+            {
+                Thread.Sleep(1500);
+                Thread thread = Thread.CurrentThread;
+                Console.WriteLine("Name of Thread: " + thread.Name + " = " + loopCount);
+            }
+        }
+    }
+}
